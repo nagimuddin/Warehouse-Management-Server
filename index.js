7ce75a48e5ec3f436286ec0391a9ac3804e6ab54
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-05-20 01:00:14 +0600

    item create

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6j8zx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const itemCollection = client.db('distributeAgent').collection('item');
+
+        app.get('/item', async(req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        
+
+       
+
+        
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('Running Bike Distribute Agent Server');
+    res.send('Running Distribute Server');
 });
 
 app.listen(port, () => {
